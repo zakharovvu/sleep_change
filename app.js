9e7a574138610f45dc294683d272f973a2357993
Slava <zakharovvu@gmail.com>
2019-05-14 13:32:47 +0300

    init

--- app.js
+++ app.js
@@ -1,73 +1,69 @@
-const first = document.getElementById('first');
-const second = document.getElementById('second');
-const third = document.getElementById('third');
-const startHorse = document.getElementById('startHorse');
-const startSelect = document.getElementById('startMin');
-const finishHorse = document.getElementById('finishHorse');
-const finishMin = document.getElementById('finishMin');
+let dateStart = document.getElementById('dateStart').value = '2019-05-14T21:00'
+let dateFinish = document.getElementById('dateFinish').value = '2019-05-15T06:00'
+let people1 = document.getElementById('people1')
+let people2 = document.getElementById('people2')
+let people3 = document.getElementById('people3')
 
-const arrFinishHorse = createSelect(24, 6);
-const arrStartHorse = createSelect(24, 21);
-const arrFinishMin = createSelect(60, 30);
-const arrStartMin = createSelect(60, 0);
+document.addEventListener('click', (e) => handler(e))
 
-function createSelect(el1, el2) {
-    let result = `${Array(el1).fill('').map((el, i) => {
-        return i === el2 ? `<option selected>${i}</option>` : `<option>${i}</option>`
-    })}`
-    return result;
+document.addEventListener('change', (e) => {
+    if (e.target.id === 'dateStart') dateStart = e.target.value
+    if (e.target.id === 'dateFinish') dateFinish = e.target.value
+    handler(e)
+})
+
+function handler(e) {
+    if (e.target.id === 'button' || e.target.id === 'dateStart' || e.target.id === 'dateFinish') {
+        let difference = Date.parse(dateFinish) - Date.parse(dateStart)
+        if (difference < 0) return alert('Error: Дата начала не может быль больше даты конца.')  
+
+        people1.innerHTML = `>>>${getDateForPeople1(difference, dateStart)[0].toLocaleString().slice(11, 17)}
+            ---${getDateForPeople1(difference, dateStart)[1].toLocaleString().slice(11, 17)}`
+        people2.innerHTML = `>>>${getDateForPeople2(difference, dateStart, dateFinish)[0].toLocaleString().slice(11, 17)}
+            ---${getDateForPeople2(difference, dateStart, dateFinish)[1].toLocaleString().slice(11, 17)}
+            ||${getDateForPeople2(difference, dateStart, dateFinish)[2].toLocaleString().slice(11, 17)}
+            ---${getDateForPeople2(difference, dateStart, dateFinish)[3].toLocaleString().slice(11, 17)}`
+        people3.innerHTML = `>>>${getDateForPeople3(difference, dateFinish)[0].toLocaleString().slice(11, 17)}
+            ---${getDateForPeople3(difference, dateFinish)[1].toLocaleString().slice(11, 17)}
+            <br>Каждому по: ${convertMS(difference / 3 * 2)}`
+    }
 }
 
-startHorse.innerHTML = arrStartHorse; startMin.innerHTML = arrStartMin;
-finishHorse.innerHTML = arrFinishHorse; finishMin.innerHTML = arrFinishMin;
-    
-function result() {
-    let dataStart = new Date(); dataStart.setHours(startHorse.value); dataStart.setMinutes(startMin.value);
-    let dataFinish = new Date(); dataFinish.setHours(finishHorse.value); dataFinish.setMinutes(finishMin.value); 
-    let x = 0;
-    +startHorse.value > +finishHorse.value ? x = 1 : x = 0;
-    dataFinish.setDate(new Date().getDate() + parseInt(x));
 
-    first.innerHTML = resultForFirst();
-    second.innerHTML = resultForSecond();
-    third.innerHTML = resultForThird();
+function getDateForPeople1(difference, dateStart) {
+    let start = getDateIsMilisec(Date.parse(dateStart))
+    let finish = getDateIsMilisec(Date.parse(dateStart) + difference / 3 * 2)
+    return [start, finish]
+}
 
-    function resultForFirst() {
-        let difference =  dataFinish.getTime() - dataStart.getTime();
-        let res = dataStart.getTime() + (difference / 3) * 2;
-        return `${dataStart.toString().substr(16, 05)} 
-            --- ${(new Date(1970, 0, 1, 2, 0, 0, res)).toString().substr(16, 05)}`;   
-    }
+function getDateForPeople2(difference, dateStart, dateFinish) {
+    let start1 = getDateIsMilisec(Date.parse(dateStart))
+    let finish1 = getDateIsMilisec(Date.parse(dateStart) + (difference / 3 * 2) / 2)
+    let start2 = getDateIsMilisec(Date.parse(dateFinish) - (difference / 3 * 2) / 2)
+    let finish2 = getDateIsMilisec(Date.parse(dateFinish))
+    return [start1, finish1, start2, finish2]
+}
 
-    function resultForSecond() {
-        let difference = dataFinish.getTime() - dataStart.getTime();
-        let res = dataStart.getTime() + ((difference / 3) * 2) / 2;
-        let res2 = dataStart.getTime() + (difference / 3) * 2;
-        return `${dataStart.toString().substr(16, 05)} 
-            --- ${(new Date(1970, 0, 1, 2, 0, 0, res)).toString().substr(16, 05)} ||
-            ${(new Date(1970, 0, 1, 2, 0, 0, res2)).toString().substr(16, 05)} 
-            --- ${dataFinish.toString().substr(16, 05)}`;   
-    }
+function getDateForPeople3(difference, dateFinish) {
+    let start = getDateIsMilisec(Date.parse(dateFinish) - difference / 3 * 2)
+    let finish = getDateIsMilisec(Date.parse(dateFinish))
+    return [start, finish]
+}
 
-    function resultForThird() {
-        let difference = dataFinish.getTime() - dataStart.getTime();
-        let res = dataFinish.getTime() - (difference / 3) * 2;
-        let horse = Math.round((difference / 3) * 2 / 100 / 60 / 60);
-        let min =  (difference / 3) * 2 / 100 / 60 / 60 / 60;
-        return `${(new Date(1970, 0, 1, 2, 0, 0, res)).toString().substr(16, 05)} 
-            --- ${dataFinish.toString().substr(16, 05)}<br><br>
-           Каждому получется: ${convertMS((difference / 3) * 2)}`;   
-    }
+function getDateIsMilisec(ms) {
+    let d = new Date(1970, 0, 1, 0, 0, 0, ms)
+    d.setHours(d.getHours() + 3)
+    return d
+}
 
-    function convertMS(ms) {
-        var d, h, m, s;
-        s = Math.floor(ms / 1000);
-        m = Math.floor(s / 60);
-        h = Math.floor(m / 60);
-        m = m % 60;
-        d = Math.floor(h / 24);
-        h = h % 24;
-        h += d * 24;
-        return h + ':' + m;
-    }
+function convertMS(ms) {
+    var d, h, m, s;
+    s = Math.floor(ms / 1000);
+    m = Math.floor(s / 60);
+    h = Math.floor(m / 60);
+    m = m % 60;
+    d = Math.floor(h / 24);
+    h = h % 24;
+    h += d * 24;
+    return h + ':' + m;
 }
